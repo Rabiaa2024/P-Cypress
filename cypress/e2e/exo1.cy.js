6de2bf6e1cf7bdefd0e3d3c219e6bf9d42dfadc8
Rabia <rabi.malek88@gmail.com>
2024-10-30 16:29:44 +0100

    ProjetCy

---
+++ cypress/e2e/exo1.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe("Autentification nominale", function () {
+  it("connexion OK", function () {
+    cy.visit("https://www.saucedemo.com/");
+    cy.get('[data-test="username"]')
+      .type("standard_user")
+      .should("have.value", "standard_user");
+    cy.get('[data-test="password"]')
+      .type("secret_sauce")
+      .should("have.value", "secret_sauce");
+    cy.get('[data-test="login-button"]').click();
+    cy.url().should("include", "/inventory.html");
+    cy.get('[data-test="inventory-item-name"]').should("be.visible");
+  });
+
+  it("connexion KO", function () {
+    cy.visit("https://www.saucedemo.com/");
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error-button"]')
+      .should("be.visible")
+      .and(
+        "contain",
+        "Epic sadface: Username and password do not match any user in this service"
+      );
+  });
+});
